13be5c84c220d0f233d8a0805b02c1f2a1907498
fikadu <fikadu026@gmail.com>
2025-02-10 21:25:04 +0200

    cmt

--- server/app.js
+++ server/app.js
@@ -1,20 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+import { PORT, NODE_ENV } from "./config/env.js";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${(PORT, NODE_ENV)}`);
+})
